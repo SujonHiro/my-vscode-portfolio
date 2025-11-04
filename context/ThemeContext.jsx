2ab9b8a794ff5dd722cf457594ee1e3c0ea8e842
Sujon <sujonhowlader67@gmail.com>
2025-11-04 13:39:55 +0600

    store theme

--- context/ThemeContext.jsx
+++ context/ThemeContext.jsx
@@ -7,14 +7,14 @@ export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("github");
 
   useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-    localStorage.setItem("theme", theme);
-  }, [theme]);
-
-  useEffect(() => {
     const saved = localStorage.getItem("theme");
     if (saved) setTheme(saved);
   }, []);
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   return <ThemeContext value={{ theme, setTheme }}>{children}</ThemeContext>;
 };
